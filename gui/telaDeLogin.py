76086e8567619097bb953240a0027cbc08d28919
Hemenson David <enzobr44@gmail.com>
2019-08-03 13:17:32 -0300

    modificacao na tela de login

--- gui/telaDeLogin.py
+++ gui/telaDeLogin.py
@@ -10,31 +10,44 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 import sys
 
-
 class Ui_mainFrame(object):
     def setupUi(self, mainFrame):
         mainFrame.setObjectName("mainFrame")
         mainFrame.resize(528, 385)
         self.centralwidget = QtWidgets.QWidget(mainFrame)
         self.centralwidget.setObjectName("centralwidget")
+
         self.lbLogin = QtWidgets.QLabel(self.centralwidget)
         self.lbLogin.setGeometry(QtCore.QRect(50, 220, 61, 41))
         font = QtGui.QFont()
         font.setPointSize(11)
         self.lbLogin.setFont(font)
         self.lbLogin.setObjectName("lbLogin")
+
         self.lbSenha = QtWidgets.QLabel(self.centralwidget)
         self.lbSenha.setGeometry(QtCore.QRect(50, 250, 71, 31))
         font = QtGui.QFont()
         font.setPointSize(11)
         self.lbSenha.setFont(font)
         self.lbSenha.setObjectName("lbSenha")
+
         self.tfLogin = QtWidgets.QLineEdit(self.centralwidget)
         self.tfLogin.setGeometry(QtCore.QRect(100, 230, 113, 20))
         self.tfLogin.setObjectName("tfLogin")
+
         self.tfSenha = QtWidgets.QLineEdit(self.centralwidget)
         self.tfSenha.setGeometry(QtCore.QRect(100, 260, 113, 20))
         self.tfSenha.setObjectName("tfSenha")
+
+        self.lcdRelogio = QtWidgets.QLCDNumber(self.centralwidget)
+        self.lcdRelogio.setGeometry(QtCore.QRect(410, 300, 111, 41))
+        self.lcdRelogio.setSmallDecimalPoint(False)
+        self.lcdRelogio.setObjectName("lcdRelogio")
+
+        self.btEntrar = QtWidgets.QPushButton(self.centralwidget)
+        self.btEntrar.setGeometry(QtCore.QRect(150, 290, 61, 29))
+        self.btEntrar.setObjectName("btEntrar")
+
         mainFrame.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(mainFrame)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 528, 23))
@@ -70,13 +83,13 @@ class Ui_mainFrame(object):
         mainFrame.setWindowTitle(_translate("mainFrame", "Login"))
         self.lbLogin.setText(_translate("mainFrame", "Login:"))
         self.lbSenha.setText(_translate("mainFrame", "Senha:"))
+        self.btEntrar.setText(_translate("mainFrame", "Entrar"))
         self.mnArquivo.setTitle(_translate("mainFrame", "Arquivo"))
         self.mnSobre.setTitle(_translate("mainFrame", "Sobre"))
         self.miAjuda.setText(_translate("mainFrame", "Ajuda"))
         self.miSalvar.setText(_translate("mainFrame", "Salvar"))
         self.miLimpar.setText(_translate("mainFrame", "Limpar"))
         self.miCarregar.setText(_translate("mainFrame", "Carregar"))
-
 
 def iniciarTela():
     app = QtWidgets.QApplication(sys.argv)
